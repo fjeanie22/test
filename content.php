277db005fbe94263d2df189f9f7c5e651c6ba81b
Jeanie <33681805+fjeanie22@users.noreply.github.com>
2019-02-27 22:35:42 +0200

    Update content.php

--- content.php
+++ content.php
@@ -7,14 +7,20 @@
 
 get_header(); ?>
 
+  
+
 	<div id="primary"  <?php echo of_get_option( 'site_layout' ); ?>">
 		<main id="main" class="site-main" role="main">
-<?php $acsessuar = new WP_Query( array( 'post_type' => 'acme_product', 'posts_per_page' => 10 ) ); ?>
+<?php $acsessuar = new WP_Query( array( 'post_type' => 'acme_product', 'posts_per_page' => 5 ) ); ?>
+
+
 
  <?php while ( $acsessuar->have_posts() ) : $acsessuar->the_post(); ?>
 		
 
-			<?php get_template_part( 'content', 'single' ); ?>
+			
+			<?php get_template_part( 'content', 'single');  ?>
+			
 
 			
 
